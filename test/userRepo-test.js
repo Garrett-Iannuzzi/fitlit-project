f685ae2bce6af4320da1863130391aa6fa0f7b2e
Garrett Iannuzzi <garrettiannuzzi@gmail.com>
2019-10-08 14:36:28 -0600

    Add testing for userRepo and methods for get user and step goal

--- test/userRepo-test.js
+++ test/userRepo-test.js
@@ -20,4 +20,24 @@ describe('UserRepo', () => {
     expect(userRepo.userData).to.eql(sampleUserData);
   });
 
+  it('should be able to get a user by id', () => {
+    expect(userRepo.getUserInfo(3)).to.eql({
+      "id": 3,
+      "name": "Herminia Witting",
+      "address": "85823 Bosco Fork, East Oscarstad MI 85126-5660",
+      "email": "Elwin.Tromp@yahoo.com",
+      "strideLength": 4.4,
+      "dailyStepGoal": 5000,
+      "friends": [
+        19,
+        11,
+        42,
+        33
+      ]
+    });
+  });
+
+  it('should be able to get average step goal for all users', () => {
+    expect(userRepo.getAverageStepGoalAllUsers()).to.equal(6400);
+  });
 });
