0c2e586a4b7e054b84082634ad2671485bd18995
Garrett Iannuzzi <garrettiannuzzi@gmail.com>
2019-10-08 22:15:53 -0600

    Add test and method for average hours slept per day on sleep class

--- src/sleep.js
+++ src/sleep.js
@@ -1,7 +1,18 @@
 class Sleep {
-  constructor() {
-
+  constructor(sleepInfo) {
+    this.sleepInfo = sleepInfo;
   }
+
+  getAvgHrsSleptPerDay() {
+    let avgSleep = this.sleepInfo.reduce((acc, cv) => {
+      acc += cv.hoursSlept / this.sleepInfo.length
+      return acc
+    }, 0)
+    return Number(avgSleep.toFixed(2))
+  }
+
+
+
 }
 
 if (typeof module !== 'undefined') {
