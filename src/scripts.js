79615559be32f591b733d9b6974e25d4d5f6f6e9
Garrett Iannuzzi <garrettiannuzzi@gmail.com>
2019-10-10 14:38:30 -0600

    Add set up for scripts file

--- src/scripts.js
+++ src/scripts.js
@@ -1,12 +1,28 @@
 const randomUser = Math.ceil(Math.random() * 50);
-const userRepo = new UserRepo(userData);
-const user = new User(userRepo.getUserInfo(randomUser));
-console.log(user)
-const hydrationRepo = new HydrationRepo(hydrationData);
-const hydration = new Hydration(hydrationRepo.getUserByID(randomUser));
-const sleepRepo = new SleepRepo(sleepData);
-const sleep = new Sleep(sleepRepo.getUserById(randomUser));
+let userRepo = new UserRepo(userData);
+let user = new User(userRepo.getUserInfo(randomUser));
+let hydrationRepo = new HydrationRepo(hydrationData);
+let hydration = new Hydration(hydrationRepo.getUserByID(randomUser));
+
+
+$(document).ready(function () {
+    userHandler();
+    hydrationHandler();
+  })
+  
+
+function userHandler() {
+  $('#span__user--info--js').text(`${user.getUserFirstName()}`);
+  
+}
+
+function hydrationHandler() {
+  $('#span__todays--water').text(`${hydration.getOuncesPerDayByDate('2019/06/17')}oz`);
+}
+
+
+// const sleepRepo = new SleepRepo(sleepData);
+// const sleep = new Sleep(sleepRepo.getUserById(randomUser));
 // const activityRepo = new ActivityRepo(activityData);
 // const activity = new Activity(activityRepo.)
 
-$('#span__todays--water').on('load').text(`${hydration.getOuncesPerDayByDate('2019/06/17')}oz`);
