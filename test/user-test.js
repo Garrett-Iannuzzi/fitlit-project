3a8309b4232e2c8640d8e9445d02a77d3f9bc365
Heather Faerber <heatherdfaerber@gmail.com>
2019-10-13 14:45:38 -0600

    Add test for find friends method

--- test/user-test.js
+++ test/user-test.js
@@ -42,15 +42,22 @@ describe('User', () => {
 
   it('should have a list of friends', () => {
     expect(user.friends).to.eql([
-      19,
-      11,
-      42,
-      33
+      2,
+      1,
+      4
     ]);
   });
 
-  it('should tell us user first name', () => {
+  it('should tell us users first name', () => {
     expect(user.getUserFirstName()).to.equal('Herminia');
+  });
+
+  it('should find all users friends', () => {
+    expect(user.findFriendNames()).to.eql([
+      'Jarvis',
+      'Luisa',
+      'Mae'
+    ]);
   });
 
 })
