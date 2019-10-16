0b943c7232730e11c9070212c6620531566732dd
Garrett Iannuzzi <garrettiannuzzi@gmail.com>
2019-10-16 08:43:02 -0600

    Add style for header

--- src/scripts.js
+++ src/scripts.js
@@ -270,8 +270,7 @@ let usersleepHoursByWeekChart = new Chart($('#sleep__week--hours--js'), {
       },
       responsive: true,
       maintainAspectRatio: false,
-  }
-});
+  });
 
 let stairsByWeekChart = new Chart($('#chart__weekly--stairs--js'), {
     type: 'line',
